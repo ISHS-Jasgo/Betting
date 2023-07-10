19ace0dff76bdad9e8b3144e4be71dbde32e20b5
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-07-10 12:10:31 +0900

    80

--- src/main.ts
+++ src/main.ts
@@ -146,6 +146,6 @@ setInterval(() => {
     io.emit('update', { games: gameData });
 }, 1000);
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(80, () => {
+    console.log('listening on *:80');
 });
